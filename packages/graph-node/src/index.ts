0451df851593c297471f3dbd3bc8ad446bc4075e
nabarun <nabarun@deepstacksoft.com>
2021-09-17 20:02:15 +0530

    Code for running wasm file.

--- packages/graph-node/src/index.ts
+++ packages/graph-node/src/index.ts
@@ -1,3 +1,14 @@
 //
 // Copyright 2021 Vulcanize, Inc.
 //
+
+import fs from 'fs/promises';
+import loader from '@assemblyscript/loader';
+
+const imports = { /* imports go here */ };
+
+export const getExports = async (filePath: string): Promise<loader.ResultObject> => {
+  const buffer = await fs.readFile(filePath);
+  const { instance, module } = loader.instantiateSync(buffer, imports);
+  return { instance, module };
+};
