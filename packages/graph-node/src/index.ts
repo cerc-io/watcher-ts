fc071affcd03db8b655839a44d6995f473b685ba
Ashwin Phatak <ashwinpphatak@gmail.com>
2021-09-20 16:16:29 +0530

    WASM instance setup (#11)
    * Code for running wasm file.
    
    * Add test for wasm exported function.
    
    * Use target names for build files.
    
    Co-authored-by: nabarun <nabarun@deepstacksoft.com>

--- packages/graph-node/src/index.ts
+++ packages/graph-node/src/index.ts
@@ -1,3 +1,13 @@
 //
 // Copyright 2021 Vulcanize, Inc.
 //
+
+import fs from 'fs/promises';
+import loader from '@assemblyscript/loader';
+
+const imports = { /* imports go here */ };
+
+export const getExports = async (filePath: string): Promise<loader.ResultObject & { exports: any }> => {
+  const buffer = await fs.readFile(filePath);
+  return loader.instantiate(buffer, imports);
+};
