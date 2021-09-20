f44fbb174f2bfb94c417fcf63174eef03a9dcde0
Ashwin Phatak <ashwinpphatak@gmail.com>
2021-09-20 16:16:29 +0530

    WASM instance setup (#11)
    * Code for running wasm file.
    
    * Add test for wasm exported function.
    
    * Use target names for build files.
    
    Co-authored-by: nabarun <nabarun@deepstacksoft.com>

---
+++ packages/graph-node/src/index.test.ts
@@ -0,0 +1,17 @@
+//
+// Copyright 2021 Vulcanize, Inc.
+//
+
+import { expect } from 'chai';
+import path from 'path';
+import 'mocha';
+
+import { getExports } from './index';
+
+const WASM_FILE_PATH = '../build/debug.wasm';
+
+it('should execute exported function', async () => {
+  const filePath = path.resolve(__dirname, WASM_FILE_PATH);
+  const { exports } = await getExports(filePath);
+  expect(exports.add(1, 2)).to.equal(3);
+});
