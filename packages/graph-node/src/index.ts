57363a14c2331e2d2ead18633688385ae3cfe642
Ashwin Phatak <ashwinpphatak@gmail.com>
2021-09-22 18:25:53 +0530

    Host API stubs

--- packages/graph-node/src/index.ts
+++ packages/graph-node/src/index.ts
@@ -5,9 +5,76 @@
 import fs from 'fs/promises';
 import loader from '@assemblyscript/loader';
 
-const imports = { /* imports go here */ };
+const imports = {
+  index: {
+    'store.get': () => {
+      console.log('store.get');
+    },
+    'store.set': () => {
+      console.log('store.set');
+    },
 
-export const getExports = async (filePath: string): Promise<loader.ResultObject & { exports: any }> => {
+    'typeConversion.stringToH160': () => {
+      console.log('typeConversion.stringToH160');
+    },
+    'typeConversion.bytesToHex': () => {
+      console.log('typeConversion.bytesToHex');
+    },
+    'typeConversion.bytesToString': () => {
+      console.log('typeConversion.bytesToString');
+    },
+    'typeConversion.bigIntToString': () => {
+      console.log('typeConversion.bigIntToString');
+    },
+
+    'bigDecimal.fromString': () => {
+      console.log('bigDecimal.fromString');
+    },
+    'bigDecimal.times': () => {
+      console.log('bigDecimal.times');
+    },
+    'bigDecimal.dividedBy': () => {
+      console.log('bigDecimal.dividedBy');
+    },
+    'bigDecimal.plus': () => {
+      console.log('bigDecimal.plus');
+    },
+    'bigDecimal.minus': () => {
+      console.log('bigDecimal.minus');
+    },
+
+    'bigInt.plus': () => {
+      console.log('bigInt.plus');
+    },
+    'bigInt.minus': () => {
+      console.log('bigInt.minus');
+    },
+    'bigInt.times': () => {
+      console.log('bigInt.times');
+    },
+    'bigInt.dividedBy': () => {
+      console.log('bigInt.dividedBy');
+    },
+    'bigInt.mod': () => {
+      console.log('bigInt.mod');
+    },
+
+    'log.log': () => {
+      console.log('log.log');
+    },
+
+    'dataSource.create': () => {
+      console.log('dataSource.create');
+    }
+  },
+  ethereum: {
+    'ethereum.call': () => {
+      console.log('ethereum.call');
+    }
+  }
+};
+
+export const instantiate = async (filePath: string): Promise<loader.ResultObject & { exports: any }> => {
   const buffer = await fs.readFile(filePath);
   return loader.instantiate(buffer, imports);
 };
