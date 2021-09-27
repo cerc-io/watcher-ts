73ce75b9e1be1108404f2561f53fd5c52c2fb90c
Ashwin Phatak <ashwinpphatak@gmail.com>
2021-09-27 13:46:46 +0530

    Eden subgraph artifacts, host API stub (#15)
    * Skip AssemblyScript during TS transpilation
    
    * Eden subgraph build files
    
    * Eden subgraph host API stubs

---
+++ packages/graph-node/src/eden.test.ts
@@ -0,0 +1,24 @@
+//
+// Copyright 2021 Vulcanize, Inc.
+//
+
+import path from 'path';
+
+import { instantiate } from './index';
+
+describe('eden wasm loader tests', () => {
+  it('should load the subgraph network wasm', async () => {
+    const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetwork/EdenNetwork.wasm');
+    await instantiate(filePath);
+  });
+
+  it('should load the subgraph network distribution wasm', async () => {
+    const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetworkDistribution/EdenNetworkDistribution.wasm');
+    await instantiate(filePath);
+  });
+
+  it('should load the subgraph network governance wasm', async () => {
+    const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetworkGovernance/EdenNetworkGovernance.wasm');
+    await instantiate(filePath);
+  });
+});
