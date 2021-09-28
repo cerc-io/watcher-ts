f61c9f1d710fd48f34e786899591ea5052d230f7
Ashwin Phatak <ashwinpphatak@gmail.com>
2021-09-28 12:28:43 +0530

    Example subgraph, ethCall test to drive implementation (#18)
    * Setup example subgraph.
    
    * Implement eth_call in subgraph.
    
    * eth-call test stub
    
    Co-authored-by: nabarun <nabarun@deepstacksoft.com>

--- packages/graph-node/src/index.ts
+++ packages/graph-node/src/index.ts
@@ -5,82 +5,94 @@
 import fs from 'fs/promises';
 import loader from '@assemblyscript/loader';
 
-const imports = {
-  index: {
-    'store.get': () => {
-      console.log('store.get');
-    },
-    'store.set': () => {
-      console.log('store.set');
-    },
-
-    'typeConversion.stringToH160': () => {
-      console.log('typeConversion.stringToH160');
-    },
-    'typeConversion.bytesToHex': () => {
-      console.log('typeConversion.bytesToHex');
-    },
-    // 'typeConversion.bytesToString': () => {
-    //   console.log('typeConversion.bytesToString');
-    // },
-    'typeConversion.bigIntToString': () => {
-      console.log('typeConversion.bigIntToString');
-    },
-
-    // 'bigDecimal.fromString': () => {
-    //   console.log('bigDecimal.fromString');
-    // },
-    // 'bigDecimal.times': () => {
-    //   console.log('bigDecimal.times');
-    // },
-    'bigDecimal.dividedBy': () => {
-      console.log('bigDecimal.dividedBy');
-    },
-    // 'bigDecimal.plus': () => {
-    //   console.log('bigDecimal.plus');
-    // },
-    // 'bigDecimal.minus': () => {
-    //   console.log('bigDecimal.minus');
-    // },
-
-    'bigInt.plus': () => {
-      console.log('bigInt.plus');
-    },
-    'bigInt.minus': () => {
-      console.log('bigInt.minus');
-    },
-    'bigInt.times': () => {
-      console.log('bigInt.times');
-    },
-    'bigInt.dividedBy': () => {
-      console.log('bigInt.dividedBy');
-    },
-    // 'bigInt.mod': () => {
-    //   console.log('bigInt.mod');
-    // },
-    'bigInt.fromString': () => {
-      console.log('bigInt.fromString');
-    },
-
-    'log.log': () => {
-      console.log('log.log');
-    },
-
-    // 'dataSource.create': () => {
-    //   console.log('dataSource.create');
-    // },
-    'dataSource.address': () => {
-      console.log('dataSource.address');
-    }
-  },
-  ethereum: {
-    'ethereum.call': () => {
-      console.log('ethereum.call');
-    }
-  }
-};
-
 export const instantiate = async (filePath: string): Promise<loader.ResultObject & { exports: any }> => {
   const buffer = await fs.readFile(filePath);
-  return loader.instantiate(buffer, imports);
+
+  const imports = {
+    index: {
+      'store.get': () => {
+        console.log('store.get');
+      },
+      'store.set': () => {
+        console.log('store.set');
+      },
+
+      'typeConversion.stringToH160': () => {
+        console.log('typeConversion.stringToH160');
+      },
+      'typeConversion.bytesToHex': () => {
+        console.log('typeConversion.bytesToHex');
+      },
+      // 'typeConversion.bytesToString': () => {
+      //   console.log('typeConversion.bytesToString');
+      // },
+      'typeConversion.bigIntToString': () => {
+        console.log('typeConversion.bigIntToString');
+      },
+
+      // 'bigDecimal.fromString': () => {
+      //   console.log('bigDecimal.fromString');
+      // },
+      // 'bigDecimal.times': () => {
+      //   console.log('bigDecimal.times');
+      // },
+      'bigDecimal.dividedBy': () => {
+        console.log('bigDecimal.dividedBy');
+      },
+      // 'bigDecimal.plus': () => {
+      //   console.log('bigDecimal.plus');
+      // },
+      // 'bigDecimal.minus': () => {
+      //   console.log('bigDecimal.minus');
+      // },
+
+      'bigInt.plus': () => {
+        console.log('bigInt.plus');
+      },
+      'bigInt.minus': () => {
+        console.log('bigInt.minus');
+      },
+      'bigInt.times': () => {
+        console.log('bigInt.times');
+      },
+      'bigInt.dividedBy': () => {
+        console.log('bigInt.dividedBy');
+      },
+      // 'bigInt.mod': () => {
+      //   console.log('bigInt.mod');
+      // },
+      'bigInt.fromString': () => {
+        console.log('bigInt.fromString');
+      },
+
+      'log.log': (_: number, msg: number) => {
+        console.log('console.log', __getString(msg));
+      },
+
+      // 'dataSource.create': () => {
+      //   console.log('dataSource.create');
+      // },
+      'dataSource.address': () => {
+        console.log('dataSource.address');
+      }
+    },
+    ethereum: {
+      'ethereum.call': () => {
+        console.log('ethereum.call');
+        return null;
+      }
+    },
+    conversion: {
+      'typeConversion.stringToH160': () => {
+        console.log('typeConversion.stringToH160');
+      }
+    }
+  };
+
+  const instance = await loader.instantiate(buffer, imports);
+
+  const exports = instance.exports;
+  const { __getString } = exports;
+
+  return instance;
 };
