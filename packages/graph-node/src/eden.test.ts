e43c5a28e1ec32c8a7ae6682e6e9c5735249f4fa
nikugogoi <nabarun@deepstacksoft.com>
2021-10-06 17:47:01 +0530

    Test for calling ethereum host API and updated eden subgraph   (#29)
    * Implement graph-ts eth-call API
    
    * Tests for eden subgraph

--- packages/graph-node/src/eden.test.ts
+++ packages/graph-node/src/eden.test.ts
@@ -6,19 +6,22 @@ import path from 'path';
 
 import { instantiate } from './index';
 
-xdescribe('eden wasm loader tests', () => {
+describe('eden wasm loader tests', () => {
   it('should load the subgraph network wasm', async () => {
     const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetwork/EdenNetwork.wasm');
-    await instantiate(filePath);
+    const { exports: { _start } } = await instantiate(filePath);
+    _start();
   });
 
   it('should load the subgraph network distribution wasm', async () => {
     const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetworkDistribution/EdenNetworkDistribution.wasm');
-    await instantiate(filePath);
+    const { exports: { _start } } = await instantiate(filePath);
+    _start();
   });
 
   it('should load the subgraph network governance wasm', async () => {
     const filePath = path.resolve(__dirname, '../test/subgraph/eden/EdenNetworkGovernance/EdenNetworkGovernance.wasm');
-    await instantiate(filePath);
+    const { exports: { _start } } = await instantiate(filePath);
+    _start();
   });
 });
