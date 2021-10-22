926179ad57874e47a49e1d0f275208b61b64eb7e
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2021-10-22 14:47:56 +0530

    Optional server port arg. for watcher generation (#284)

--- packages/codegen/src/config.ts
+++ packages/codegen/src/config.ts
@@ -12,14 +12,16 @@ const TEMPLATE_FILE = './templates/config-template.handlebars';
 /**
  * Writes the config file generated from a template to a stream.
  * @param watcherKind Watcher kind to be passed to the template.
+ * @param port Port for the watcher server.
  * @param folderName Watcher folder name to be passed to the template.
  * @param outStream A writable output stream to write the config file to.
  */
-export function exportConfig (watcherKind: string, folderName: string, outStream: Writable): void {
+export function exportConfig (watcherKind: string, port: number, folderName: string, outStream: Writable): void {
   const templateString = fs.readFileSync(path.resolve(__dirname, TEMPLATE_FILE)).toString();
   const template = Handlebars.compile(templateString);
   const config = template({
     watcherKind,
+    port,
     folderName
   });
   outStream.write(config);
