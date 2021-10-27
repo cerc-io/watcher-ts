cfab5c35505f76b59b36693daf9cec2afc863282
nikugogoi <nabarun@deepstacksoft.com>
2021-10-27 14:43:48 +0530

    Test case for EdenNetwork event handler (#31)
    * Test case for calling EdenNetwork event handler
    
    * Test cases for calling EdenNetwork event handlers
    
    * Test cases for calling event handlers in EdenNetworkDistribution
    
    * Test cases for calling event handlers in EdenNetworkGovernance
    
    * Perform eth-calls according to contract name in param
    
    * Pass function params in eth-call host API
    
    * Return value from eth-call to wasm
    
    * Add instructions for running tests
    
    * Install assemblyscript from github packages
    
    * Add instrcutions for installing private github packages
    
    * Use vulcanize assemblyscript package

--- packages/graph-node/src/eth-call.test.ts
+++ packages/graph-node/src/eth-call.test.ts
@@ -2,16 +2,30 @@
 // Copyright 2021 Vulcanize, Inc.
 //
 
+import assert from 'assert';
 import path from 'path';
 
 import { instantiate } from './index';
+import exampleAbi from '../test/subgraph/example1/build/Example1/abis/Example1.json';
 
 describe('eth-call wasm tests', () => {
   let exports: any;
 
+  const contractAddress = process.env.EXAMPLE_CONTRACT_ADDRESS;
+  assert(contractAddress);
+
+  const data = {
+    abis: {
+      Example1: exampleAbi
+    },
+    dataSource: {
+      address: contractAddress
+    }
+  };
+
   it('should load the subgraph example wasm', async () => {
     const filePath = path.resolve(__dirname, '../test/subgraph/example1/build/Example1/Example1.wasm');
-    const instance = await instantiate(filePath);
+    const instance = await instantiate(filePath, data);
     exports = instance.exports;
   });
 
