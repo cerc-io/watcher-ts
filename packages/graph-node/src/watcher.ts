d29eb32a23332cebf50d58e99d6240c7c2160737
nikugogoi <nabarun@deepstacksoft.com>
2021-11-22 15:46:29 +0530

    Implement gql queries for relation entities similar to subgraph (#53)
    * Add implementation for one to one relation
    
    * Implement one to many relation in gql queries
    
    * Make changes for gql relation queries in eden-watcher
    
    * Implement subgraph gql relation queries with joins

--- packages/graph-node/src/watcher.ts
+++ packages/graph-node/src/watcher.ts
@@ -184,9 +184,9 @@ export class GraphWatcher {
     this._indexer = indexer;
   }
 
-  async getEntity<Entity> (entity: new () => Entity, id: string, blockHash: string): Promise<any> {
+  async getEntity<Entity> (entity: new () => Entity, id: string, blockHash: string, relations: { [key: string]: any }): Promise<any> {
     // Get entity from the database.
-    const result = await this._database.getEntity(entity, id, blockHash) as any;
+    const result = await this._database.getEntityWithRelations(entity, id, blockHash, relations) as any;
 
     // Resolve any field name conflicts in the entity result.
     return resolveEntityFieldConflicts(result);
