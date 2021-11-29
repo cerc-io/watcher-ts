4eb2077277ead17bff230ee48b2ff7bc5faf2a12
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2021-11-29 17:51:16 +0530

    Update graph-node tests to use dummy indexer (#65)
    * Update test to call handlers to use dummy indexer
    
    * Update graph-node tests to use dummy indexer

--- packages/graph-node/src/call-handler.test.ts
+++ packages/graph-node/src/call-handler.test.ts
@@ -7,11 +7,12 @@ import chai, { assert, expect } from 'chai';
 import spies from 'chai-spies';
 import { utils } from 'ethers';
 
-import { getDummyEventData, getTestDatabase } from '../test/utils';
+import { getDummyEventData, getDummyGraphData, getTestDatabase, getTestIndexer } from '../test/utils';
 import abi from '../test/subgraph/example1/build/Example1/abis/Example1.json';
 import { instantiate } from './loader';
 import { createEvent, createBlock, Block } from './utils';
 import { Database } from './database';
+import { Indexer } from '../test/utils/indexer';
 
 chai.use(spies);
 
@@ -20,12 +21,21 @@ const sandbox = chai.spy.sandbox();
 describe('call handler in mapping code', () => {
   let exports: any;
   let db: Database;
+  let indexer: Indexer;
 
-  // Create dummy event data.
+  // Create dummy test data.
   const dummyEventData = getDummyEventData();
+  const dummyGraphData = getDummyGraphData();
 
   before(async () => {
     db = getTestDatabase();
+    indexer = getTestIndexer();
+
+    sandbox.on(indexer, 'createDiffStaged', (contractAddress: string, blockHash: string, data: any) => {
+      assert(contractAddress);
+      assert(blockHash);
+      assert(data);
+    });
 
     sandbox.on(db, 'getEntity', (blockHash: string, entityString: string, idString: string) => {
       assert(blockHash);
@@ -34,20 +44,12 @@ describe('call handler in mapping code', () => {
     });
 
     sandbox.on(db, 'fromGraphEntity', async (instanceExports: any, block: Block, entity: string, entityInstance: any) => {
-      const entityFields = [
-        { type: 'varchar', propertyName: 'id' },
-        { type: 'bigint', propertyName: 'count' },
-        { type: 'varchar', propertyName: 'paramString' },
-        { type: 'integer', propertyName: 'paramInt' },
-        { type: 'bigint', propertyName: 'paramBigInt' },
-        { type: 'boolean', propertyName: 'paramBoolean' },
-        { type: 'varchar', propertyName: 'paramBytes' },
-        { type: 'numeric', propertyName: 'paramBigDecimal' },
-        { type: 'varchar', propertyName: 'related' },
-        { type: 'varchar', propertyName: 'manyRelated' }
-      ];
+      assert(instanceExports);
+      assert(block);
+      assert(entity);
+      assert(entityInstance);
 
-      return db.getEntityValues(instanceExports, block, entityInstance, entityFields);
+      return {};
     });
 
     sandbox.on(db, 'saveEntity', (entity: string, data: any) => {
@@ -58,19 +60,23 @@ describe('call handler in mapping code', () => {
 
   it('should load the subgraph example wasm', async () => {
     const filePath = path.resolve(__dirname, '../test/subgraph/example1/build/Example1/Example1.wasm');
-    const instance = await instantiate(db, { event: { block: dummyEventData.block } }, filePath);
+    const instance = await instantiate(
+      db,
+      indexer,
+      { event: { block: dummyEventData.block } },
+      filePath,
+      dummyGraphData
+    );
     exports = instance.exports;
-  });
-
-  it('should execute the event handler function', async () => {
-    const {
-      _start,
-      handleTest
-    } = exports;
+    const { _start } = exports;
 
     // Important to call _start for built subgraphs on instantiation!
     // TODO: Check api version https://github.com/graphprotocol/graph-node/blob/6098daa8955bdfac597cec87080af5449807e874/runtime/wasm/src/module/mod.rs#L533
     _start();
+  });
+
+  it('should execute the event handler function', async () => {
+    const { handleTest } = exports;
 
     // Create event params data.
     const contractInterface = new utils.Interface(abi);
@@ -94,15 +100,12 @@ describe('call handler in mapping code', () => {
     expect(db.getEntity).to.have.been.called();
     expect(db.fromGraphEntity).to.have.been.called();
     expect(db.saveEntity).to.have.been.called();
+    expect(indexer.createDiffStaged).to.have.been.called();
   });
 
   it('should execute the block handler function', async () => {
-    const { _start, handleBlock } = exports;
+    const { handleBlock } = exports;
     const blockData = dummyEventData.block;
-
-    // Important to call _start for built subgraphs on instantiation!
-    // TODO: Check api version https://github.com/graphprotocol/graph-node/blob/6098daa8955bdfac597cec87080af5449807e874/runtime/wasm/src/module/mod.rs#L533
-    _start();
 
     // Create an ethereum block to be passed to the handler.
     const block = await createBlock(exports, blockData);
