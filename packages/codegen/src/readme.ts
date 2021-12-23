e883463aa6c0225a81e1e5842c8d0866e72aea60
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2021-12-23 17:26:03 +0530

    Parse events for multiple contracts in the generated code (#95)
    * Parse events for multiple contracts in the generated code
    
    * Use contract wise artifacts in the generated indexer methods
    
    * Update codegen docs to use config file to generate a watcher
    
    * Add watcher generation config to eden-watcher

--- packages/codegen/src/readme.ts
+++ packages/codegen/src/readme.ts
@@ -12,15 +12,15 @@ const TEMPLATE_FILE = './templates/readme-template.handlebars';
 /**
  * Writes the README.md file generated from a template to a stream.
  * @param folderName Watcher folder name to be passed to the template.
- * @param contractName Input contract name given as title of the README.
+ * @param port Watcher server port.
  * @param outStream A writable output stream to write the README.md file to.
  */
-export function exportReadme (folderName: string, contractName: string, outStream: Writable): void {
+export function exportReadme (folderName: string, port: number, outStream: Writable): void {
   const templateString = fs.readFileSync(path.resolve(__dirname, TEMPLATE_FILE)).toString();
   const template = Handlebars.compile(templateString);
   const readmeString = template({
     folderName,
-    contractName
+    port
   });
   outStream.write(readmeString);
 }
