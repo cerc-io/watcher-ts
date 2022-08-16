cd543ed40a5c3a981c58a701def3a1e87b03d948
prathamesh0 <prathamesh.musale0@gmail.com>
2022-08-16 17:38:22 +0530

    Remove Uniswap watchers and related code

--- packages/uni-info-watcher/src/types/common/main.d.ts
+++
@@ -1,6 +0,0 @@
-//
-// Copyright 2021 Vulcanize, Inc.
-//
-
-// https://medium.com/@steveruiz/using-a-javascript-library-without-type-declarations-in-a-typescript-project-3643490015f3
-declare module 'canonical-json'
--- packages/util/test/index.ts
+++
@@ -1,5 +0,0 @@
-//
-// Copyright 2021 Vulcanize, Inc.
-//
-
-export * from './actions';
