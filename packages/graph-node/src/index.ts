3d4518ece1ef1fb1415e11ce68ef52277a3775b3
nabarun <nabarun@deepstacksoft.com>
2022-11-25 10:45:35 +0530

    Import GraphDatabase from util instead of graph-node

--- packages/graph-node/src/index.ts
+++ packages/graph-node/src/index.ts
@@ -1,11 +1,2 @@
-export {
-  ENTITY_QUERY_TYPE,
-  GraphDatabase,
-  prepareEntityState,
-  updateEntitiesFromState,
-  resolveEntityFieldConflicts,
-  afterEntityInsertOrUpdate
-} from '@cerc-io/util';
-
 export * from './state-utils';
 export * from './watcher';
