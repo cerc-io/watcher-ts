d5a1f38db491e8329d38dff24cb0d8b41f90187e
nabarun <nabarun@deepstacksoft.com>
2022-12-21 14:31:32 +0530

    Add react app and use peer package for broadcasting

--- packages/peer/src/index.ts
+++ packages/peer/src/index.ts
@@ -6,7 +6,7 @@ import { createLibp2p, Libp2p } from 'libp2p'
 // For nodejs.
 import wrtc from 'wrtc'
 import assert from 'assert'
-import { pipe } from 'it-pipe'
+import { pipe, Source } from 'it-pipe'
 import * as lp from 'it-length-prefixed'
 import map from 'it-map'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
@@ -16,26 +16,27 @@ import { webSockets } from '@libp2p/websockets'
 import { webRTCStar, WebRTCStarTuple } from '@libp2p/webrtc-star'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
-import type { Stream } from '@libp2p/interface-connection'
+import type { Stream as P2PStream } from '@libp2p/interface-connection'
 import type { PeerInfo } from '@libp2p/interface-peer-info'
 import { PeerId } from '@libp2p/interface-peer-id'
 
 const PROTOCOL = '/chat/1.0.0';
-const SIGNAL_SERVER_URL = '/ip4/127.0.0.1/tcp/13579/wss/p2p-webrtc-star';
+const DEFAULT_SIGNAL_SERVER_URL = '/ip4/127.0.0.1/tcp/13579/wss/p2p-webrtc-star';
 
 export class Peer {
+  _messageSource?: Source<string>
   _node?: Libp2p
   _wrtcStar: WebRTCStarTuple
+  _remotePeerIds: PeerId[] = []
 
   constructor () {
     // Instantiation in nodejs.
     this._wrtcStar = webRTCStar({ wrtc });
-
-    // Read utf-8 from stdin
-    process.stdin.setEncoding('utf8')
   }
 
-  async init () {
+  async init (signalServerURL = DEFAULT_SIGNAL_SERVER_URL, messageSource: Source<string>) {
+    this._messageSource = messageSource;
+  
     this._node = await createLibp2p({
       addresses: {
         // Add the signaling server address, along with our PeerId to our multiaddrs list
@@ -44,7 +45,7 @@ export class Peer {
         listen: [
           // '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
           // '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star'
-          SIGNAL_SERVER_URL
+          signalServerURL
         ]
       },
       transports: [
@@ -63,17 +64,23 @@ export class Peer {
 
     this._node.addEventListener('peer:discovery', (evt) => {
       const peer = evt.detail
-      this.connectPeer(peer)
+
+      if (!this._remotePeerIds.some(remotePeerId => remotePeerId.toString() === peer.id.toString())) {
+        this._connectPeer(peer)
+      }
     })
     
     this._node.connectionManager.addEventListener('peer:connect', (evt) => {
-      console.log('Connected to %s', evt.detail.remotePeer.toString()) // Log connected peer
+      const remotePeerId = evt.detail.remotePeer;
+      this._remotePeerIds.push(remotePeerId)
+      console.log('Connected to %s', remotePeerId.toString()) // Log connected peer
     })
 
     // Listen for peers disconnecting
     this._node.connectionManager.addEventListener('peer:disconnect', (evt) => {
-      const connection = evt.detail
-      console.log(`Disconnected from ${connection.remotePeer.toString()}`)
+      const remotePeerId = evt.detail.remotePeer;
+      this._remotePeerIds = this._remotePeerIds.filter(remotePeerId);
+      console.log(`Disconnected from ${remotePeerId.toString()}`)
     })
 
     // Handle messages for the protocol
@@ -84,7 +91,7 @@ export class Peer {
     console.log(`libp2p id is ${this._node.peerId.toString()}`)
   }
 
-  async connectPeer (peer: PeerInfo) {
+  async _connectPeer (peer: PeerInfo) {
     assert(this._node)
     console.log(`Found peer ${peer.id.toString()}`)
 
@@ -98,12 +105,13 @@ export class Peer {
     }
   }
 
-  _handleStream (peerId: PeerId, stream: Stream) {
+  _handleStream (peerId: PeerId, stream: P2PStream) {
+    assert(this._messageSource)
+
     // Send message to pipe from stdin
     pipe(
-      // Read from stdin (the source)
-      // TODO: Implement write stream for browser
-      process.stdin,
+      // Read from readable stream (the source)
+      this._messageSource,
       // Turn strings into buffers
       (source) => map(source, (string) => uint8ArrayFromString(string)),
       // Encode with length prefix (so receiving side knows how much data is coming)
@@ -132,8 +140,3 @@ export class Peer {
     )
   }
 }
-
-const peer = new Peer();
-
-peer.init()
-  .then(() => console.log("started"))
