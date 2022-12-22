e3b862a7aec437f3ee6bf91e0e4739c8e983722e
nabarun <nabarun@deepstacksoft.com>
2022-12-22 13:55:20 +0530

    Add logs for events with details

--- packages/peer-test-app/src/App.tsx
+++ packages/peer-test-app/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
     (async () => {
       if (peer) {
         await peer.init(process.env.REACT_APP_SIGNAL_SERVER)
+        console.log(`Peer ID is ${peer.peerId!.toString()}`);
 
         peer.subscribeMessage((peerId, message) => {
           console.log(`${peerId.toString()} > ${message}`)
@@ -23,10 +24,15 @@ function App() {
         window.broadcast = (message: string) => {
           peer.broadcastMessage(message)
         }
-  
-        console.log(`Peer ID is ${peer.peerId!.toString()}`);
       }
     })()
+
+    return () => {
+      if (peer) {
+        // TODO: Await for peer close
+        peer.close()
+      }
+    }
   }, [peer])
 
   useEffect(() => {
@@ -38,16 +44,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Peer messaging app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
